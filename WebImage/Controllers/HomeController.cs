e3b72b9cf54c80a62b7b140cead089c904fa060b
skepee <13406481+skepee@users.noreply.github.com>
2019-06-12 17:25:51 +0100

    first commit

---
+++ WebImage/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using WebImage.Models;
+
+namespace WebImage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment hostingEnv;
+
+        public HomeController(IHostingEnvironment _hostingEnv)
+        {
+            hostingEnv = _hostingEnv;
+        }
+
+        //[ResponseCache(Duration = 30, Location = ResponseCacheLocation.Client, NoStore = false)]
+        public IActionResult Index()
+        {
+            ContentModel files = new ContentModel(@"C:\Users\miacone\source\repos\WebImage\WebImage\wwwroot\images");
+            
+
+
+
+
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebImage/Models/ContentModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+//using static System.Net.Mime.MediaTypeNames;
+//using System.Drawing;
+//using System.Windows.Media.Imaging;
+
+namespace WebImage.Models
+{
+    public class ContentModel
+    {
+        public List<FileModel> Files { get; set; }
+
+
+        public ContentModel(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                foreach (var file in Directory.GetFiles(path))
+                {
+                    FileInfo f = new FileInfo(file);
+
+
+                    //BitmapSource img = BitmapFrame.Create(fs);
+                    //BitmapMetadata md = (BitmapMetadata)img.Metadata;
+
+
+                    //Image img = Image.FromFile(fileName);
+                    //BitmapSource img = BitmapFrame.Create(fs);
+
+                    //using (Image sourceImage = Image.FromStream(stream, false, false))
+                    //{
+                    //    //Console.WriteLine(sourceImage.Width);
+                    //    //Console.WriteLine(sourceImage.Height);
+                    //}
+
+
+                    Files.Add(new FileModel()
+                    {
+                        Name=f.Name,
+                        Extension=f.Extension,
+                        Length=f.Length
+                    });
+               }                
+            }
+        }
+
+    }
+
+
+
+    public class FileModel
+    {
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public long Length { get; set; }
+    }
+}
