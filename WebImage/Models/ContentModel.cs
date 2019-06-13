8a394697d94f813f41733ad92cf6db940310bed5
skepee <13406481+skepee@users.noreply.github.com>
2019-06-13 17:21:38 +0100

    Generate Json from directory

--- WebImage/Models/ContentModel.cs
+++ WebImage/Models/ContentModel.cs
@@ -1,60 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-//using static System.Net.Mime.MediaTypeNames;
-//using System.Drawing;
-//using System.Windows.Media.Imaging;
 
 namespace WebImage.Models
 {
     public class ContentModel
     {
         public List<FileModel> Files { get; set; }
+        
+        public string MaxLen { get; set; }
 
-
-        public ContentModel(string path)
+        public ContentModel(string host)
         {
+            string path= @"D:\imagefolder";
+
+            Files = new List<FileModel>();
             if (Directory.Exists(path))
             {
                 foreach (var file in Directory.GetFiles(path))
                 {
                     FileInfo f = new FileInfo(file);
 
-
-                    //BitmapSource img = BitmapFrame.Create(fs);
-                    //BitmapMetadata md = (BitmapMetadata)img.Metadata;
-
-
-                    //Image img = Image.FromFile(fileName);
-                    //BitmapSource img = BitmapFrame.Create(fs);
-
-                    //using (Image sourceImage = Image.FromStream(stream, false, false))
-                    //{
-                    //    //Console.WriteLine(sourceImage.Width);
-                    //    //Console.WriteLine(sourceImage.Height);
-                    //}
-
+                    var sizekb = Math.Round(((decimal)f.Length) / 1024, 1);
+                    var sizeMb = Math.Round(((decimal)sizekb) / 1024, 2);                    
 
                     Files.Add(new FileModel()
                     {
-                        Name=f.Name,
-                        Extension=f.Extension,
-                        Length=f.Length
+                        Title = string.Empty,
+                        Name = f.Name,
+                        Extension = f.Extension,
+                        LengthKb =  sizekb,
+                        LengthMb = sizeMb,
+                        Path = "/images/" + f.Name,
+                        Url = host +  "/images/" + f.Name
                     });
-               }                
+                }
             }
         }
-
     }
 
 
 
-    public class FileModel
+    public class FileModel: JsonModel
     {
         public string Name { get; set; }
         public string Extension { get; set; }
-        public long Length { get; set; }
+        public string Path { get; set; }
     }
+
+    public class JsonModel
+    {
+        public string Title { get; set; }
+        public decimal LengthKb { get; set; }
+        public decimal LengthMb { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class JsonData
+    {
+        public List<JsonModel> MyJson { get; set; }
+        public Statistics Stat { get; set; }
+    }
+
+    public class Statistics
+    {
+        public int Count { get; set; }
+        public decimal TotalLengthKb { get; set; }
+        public decimal TotalLengthMb { get; set; }
+        public double ElapsedTime { get; set; }
+    }
+
+
 }
