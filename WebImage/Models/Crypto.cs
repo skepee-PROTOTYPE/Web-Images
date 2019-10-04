e99f32c09592cbe9a0f94e0a027cd0b411493fc9
skepee <13406481+skepee@users.noreply.github.com>
2019-10-04 17:28:35 +0100

    Upload Files, ClientCarousel.

---
+++ WebImage/Models/Crypto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebImage.Models
+{
+    public class Crypto
+    {
+    }
+
+
+
+    //public class Startup
+    //{
+    //    public void ConfigureServices(IServiceCollection services)
+    //    {
+    //        services.AddDataProtection();
+    //    }
+    //    // ...
+    //}
+    ////Then you'll be able to inject IDataProtectionProvider instance and use it to encrypt/decrypt data:
+
+    //public class MyService : IService
+    //{
+    //    private const string Purpose = "my protection purpose";
+    //    private readonly IDataProtectionProvider _provider;
+
+    //    public MyService(IDataProtectionProvider provider)
+    //    {
+    //        _provider = provider;
+    //    }
+
+    //    public string Encrypt(string plainText)
+    //    {
+    //        var protector = _provider.CreateProtector(Purpose);
+    //        return protector.Protect(plainText);
+    //    }
+
+    //    public string Decrypt(string cipherText)
+    //    {
+    //        var protector = _provider.CreateProtector(Purpose);
+    //        return protector.Unprotect(cipherText);
+    //    }
+    //}
+
+
+
+}
