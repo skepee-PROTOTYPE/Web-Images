428eacdbf0219c8121f6677b3074de6cb44e7fb4
skepee <13406481+skepee@users.noreply.github.com>
2019-10-07 17:27:20 +0100

    Database, dbcontext.

---
+++ WebImage/Helper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebImage
+{
+    public static class Helper
+    {
+        public static byte[] byteFile(string varFilePath)
+        {
+            byte[] file;
+            using (var stream = new FileStream(varFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = new BinaryReader(stream))
+                {
+                    file = reader.ReadBytes((int)stream.Length);
+                }
+            }
+            return file;
+        }
+
+
+        public static void DownloadFile(byte[] file,string name)
+        {
+            using (var fs = new FileStream(@"D:\images\" + name , FileMode.Create, FileAccess.Write))
+                fs.Write(file, 0, file.Length);
+        }
+
+        public static string Decode(string selectedImages)
+        {
+            byte[] data = Convert.FromBase64String(selectedImages);
+            string decodedString = Encoding.UTF8.GetString(data);
+            return decodedString;
+        }
+
+        public static void Encode(string selectedFiles)
+        {
+            byte[] byt = System.Text.Encoding.ASCII.GetBytes(selectedFiles);
+            string selection = Convert.ToBase64String(byt);
+        }
+
+    }
+}
