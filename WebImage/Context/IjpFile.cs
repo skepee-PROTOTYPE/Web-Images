1920e19d2656b326a5867ee9ebf986aa40c4addf
skepee <13406481+skepee@users.noreply.github.com>
2019-10-09 17:41:31 +0100

    Upload images in db, removing elements from selection.

---
+++ WebImage/Context/IjpFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebImage.Context
+{
+    [Table("FileContent")]
+    public class IjpFile
+    {
+        [Key]
+        public int FileId { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Extension { get; set; }
+        public double LengthKB { get; set; }
+        public double LengthMB { get; set; }
+        public string Url { get; set; }
+        public string Category { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
